d5cd0ff0f953799277aa1e29990bbc94d13b7961
geoffv3 <gva@seas.upenn.edu>
2014-04-13 23:34:32 -0400

    platforms added

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/Geoffrey/CIS330 workspace/KeepUp/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/Geoffrey/CIS330 workspace/KeepUp/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/Geoffrey/CIS330 workspace/KeepUp/platforms/android/ant-build/classes.dex : \
+/Users/Geoffrey/CIS330 workspace/KeepUp/platforms/android/ant-build/classes/com/phonegap/helloworld/BuildConfig.class \
+/Users/Geoffrey/CIS330 workspace/KeepUp/platforms/android/ant-build/classes/com/phonegap/helloworld/HelloWorld.class \
+/Users/Geoffrey/CIS330 workspace/KeepUp/platforms/android/ant-build/classes/com/phonegap/helloworld/R$attr.class \
+/Users/Geoffrey/CIS330 workspace/KeepUp/platforms/android/ant-build/classes/com/phonegap/helloworld/R$drawable.class \
+/Users/Geoffrey/CIS330 workspace/KeepUp/platforms/android/ant-build/classes/com/phonegap/helloworld/R$string.class \
+/Users/Geoffrey/CIS330 workspace/KeepUp/platforms/android/ant-build/classes/com/phonegap/helloworld/R$xml.class \
+/Users/Geoffrey/CIS330 workspace/KeepUp/platforms/android/ant-build/classes/com/phonegap/helloworld/R.class \
+/Users/Geoffrey/CIS330 workspace/KeepUp/platforms/android/ant-build/dexedLibs/classes-c81ff5cd4606239ecbadc85e03d1dd19.jar \
