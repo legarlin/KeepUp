1cabc1ce018d5755cc5fce99174e2ad23051d25b
Roy Weiss <royweiss@roy-weiss-computer.local>
2014-05-03 22:24:46 -0400

    Approving evidence, need to create evidence id

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/Daniel/Desktop/Spring2014/CIS350/Workspace/KeepUp/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/Daniel/Desktop/Spring2014/CIS350/Workspace/KeepUp/platforms/android/CordovaLib/AndroidManifest.xml \
+/Users/royweiss/Documents/workspace/KeepUp/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/royweiss/Documents/workspace/KeepUp/platforms/android/CordovaLib/AndroidManifest.xml \
